c444281331491a89937c4c56370b2ab2b0e2e8e7
Bluecode77732 <doookuuu@gmail.com>
2023-07-10 17:11:17 +0900

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -1,39 +1,16 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+app.get("/", function responsive (req, res) {
+    res.send("Data");
+});
 
+/* "with the modern ES6 syntax you can save a few keystrokes by using arrow functions. src : https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-start-a-working-express-server/301519"
+app.get("/", (req, res) => {
+    res.send("Data");
+});
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
